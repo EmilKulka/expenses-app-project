ebbaf057c31efb613a4b3d638ff60b216d7b5f70
EmilKulka <226310@student.uek.krakow.pl>
2025-01-15 14:11:02 +0100

    fix: trading hooks for modules, states for services (bruh)

--- frontend/expenses-app-project-frontend/src/components/pages/ForgotPasswordPage.jsx
+++
@@ -1,9 +0,0 @@
-import ResetPasswordForm from "../forms/ForgotPasswordForm";
-function ForgotPasswordPage() {
-    return (
-        <div>
-            <ResetPasswordForm/>
-        </div>
-    )
-}
-export default ForgotPasswordPage;
--- frontend/expenses-app-project-frontend/src/components/pages/RegisterPage.jsx
+++
@@ -1,9 +0,0 @@
-import RegisterForm from "../forms/RegisterForm";
-function RegisterPage() {
-    return (
-        <div>
-            <RegisterForm></RegisterForm>
-        </div>
-    )
-}
-export default RegisterPage;
